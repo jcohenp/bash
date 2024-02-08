c1b1c9828265f23f328bab5d3dbb9272f5281a31
EC2 Default User <ec2-user@ip-172-31-27-115.ec2.internal>
2024-02-08 14:47:46 +0000

    add local user on linux system

--- add-local-user.sh
+++ add-local-user.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
 
 # execute the script as root user:
-USERID=$(uid)
+USERID=$(id -u)
 ROOTUSERID='0'
-if [[ USERID -ne ROOTUSERID]]
+if [[ $USERID -ne $ROOTUSERID ]]
 then
-    echo "the script should be execited as root user. The script will exit"
-    exit(1)
+    echo 'the script should be executed as root user. The script will exit'
+    exit 1
 fi
 
 # User informations
-read -p 'What is the username' USERNAME
-read -p 'What is the real name of the user' COMMENT
-read -p 'What is the password of the user' PASSWORD
+read -p 'What is the username ' USERNAME
+read -p 'What is the real name of the user ' COMMENT
+read -p 'What is the password of the user ' PASSWORD
 
 # User creation
-useradd 
+useradd -c "${COMMENT}" -m ${USERNAME}
+echo ${PASSWORD} | passwd --stdin ${USERNAME}
+
+if [[ ${?} -ne '0' ]]
+then
+	echo "the user was not able to be created"
+	exit 1
+fi
+# Ask to the user to change password
+passwd -e ${USERNAME}
+
+# echo username, password and host
+
+echo 
+echo "username: ${USERNAME}"
+echo "password: ${PASSWORD}"
+echo "comment: ${COMMENT}"
+echo "hostname: ${HOSTNAME}"
